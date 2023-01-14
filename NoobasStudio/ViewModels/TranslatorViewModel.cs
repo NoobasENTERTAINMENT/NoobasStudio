d12ee8805050499ec969bf854ceac246495d47a5
Zettaisha <70265306+Zettaisha@users.noreply.github.com>
2023-01-14 18:37:14 +0300

    preparing for battle

---
+++ NoobasStudio/ViewModels/TranslatorViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoobasStudio.ViewModels
+{
+    public class TranslatorViewModel : ViewModelBase
+    {
+		private string _russian;
+		public string Russian
+		{
+			get
+			{
+				return _russian;
+			}
+			set
+			{
+				_russian = value;
+				OnPropertyChanged();
+			}
+		}
+		private string _english;
+		public string English
+		{
+			get
+			{
+				return _english;
+			}
+			set
+			{
+				_english = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+}
