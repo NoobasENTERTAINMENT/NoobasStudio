aadd6de02520fb2e8345cc0211f0870d2f3e6411
Rayzerid <ugfof@mail.ru>
2023-01-23 20:47:16 +0300

    Books

--- NoobasStudio/Views/CraftGenerealView.xaml.cs
+++ NoobasStudio/Views/CraftGenerealView.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace NoobasStudio.Views
 {
@@ -27,6 +17,26 @@ namespace NoobasStudio.Views
         private void ButtonClose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Border_Drop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                string filePath = System.IO.Path.GetFullPath("book.png");
+                if (filePath.Contains("\\bin\\Debug\\"))
+                     filePath = filePath.Replace("\\bin\\Debug\\", "\\Images\\");
+                borderProfileImage.ImageSource = new BitmapImage(new Uri(filePath, UriKind.Relative));
+            }
+            catch (Exception)
+            {
+                return;
+            }
         }
     }
 }
